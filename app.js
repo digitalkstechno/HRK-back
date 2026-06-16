1014c6bb636e932dfcab9427359f42354c143bfd
manavdigitalks <manavdigitalks@gmail.com>
2026-06-16 12:09:51 +0530

    increase payload size

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
-app.use(express.json({ limit: '300mb' }));
-app.use(express.urlencoded({ extended: false, limit: '300mb' }));
+app.use(express.json({ limit: '10gb' }));
+app.use(express.urlencoded({ extended: false, limit: '10gb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
